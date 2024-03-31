eb9cf80e17c521f068dea58bca0b67c82a3c1c3a
Jingwennnn <wongjingwen1234@gmail.com>
2024-04-01 02:36:00 +0800

    admin header

--- src/pages/module5/admin.jsx
+++ src/pages/module5/admin.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './admin.css'; // Assuming you have a CSS file for styles
+import Header from './Header';
 import TopNav from './TopNav';
 
 function Admin() {
   return (
     <div>
         <TopNav />
+        <Header />
         <h1>Admin</h1>
     </div>
   );
