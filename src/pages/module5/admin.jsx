b883d20883ffde876871dfd85995256307e6724e
Jingwennnn <wongjingwen1234@gmail.com>
2024-04-01 06:08:54 +0800

    new hire list and calender

--- src/pages/module5/admin.jsx
+++ src/pages/module5/admin.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from './Header';
 import TopNav from './TopNav';
+import NewHireList from './newhirelist';
+import { MyCalendar } from './newhirelist';
 
 function Admin() {
   return (
     <div>
-        <TopNav />
-        <Header />
-        <h1>Admin</h1>
+      <TopNav />
+      <Header />
+      <div className="flex">
+        <NewHireList />
+        <div className="flex-grow" />
+        <div className="w-1/2">
+          <MyCalendar/>
+        </div>
+      </div>
+      <h1>Admin</h1>
     </div>
   );
 }
