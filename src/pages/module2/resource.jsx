583b04068f7f4a70005c4cdadd06900d467fc9ea
poisondarterz <jc.chuan.0303@gmail.com>
2024-04-01 15:10:25 +0800

    hanjie fix

--- src/pages/module2/resource.jsx
+++ src/pages/module2/resource.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 
-import './resource.css';
-
 function Resource() {
   return (
-    <div className="Resource">
-      <header className="header">
-        <h1>Training Resources Hub</h1>
+    <div className="flex flex-col h-screen bg-[#0076CE] text-white">
+      <header className="p-5">
+        <h1 className="text-4xl">Training Resources Hub</h1>
       </header>
-      <main className="main-content">
-        <div className="text-content">
-          <h2>Explore our Learning Center!</h2>
-          <p>
+      <main className="flex flex-grow p-5 space-x-5">
+        <div className="flex-1 space-y-5 max-w-xl mx-auto overflow-hidden">
+          <h2 className="text-4xl font-semibold">Explore our Learning Center!</h2>
+          <p className="text-lg">
             Access vital training materials and documents to kickstart your journey with us. Everything you need to excel in your role, all in one place.
           </p>
         </div>
-        <div className="image-content">
-          <img src="url('/module2Photo.jpg')" alt="Training Resources" />
+        <div className="flex-1">
+          <img src="/module2Photo.jpg" alt="Training Resources" />
         </div>
       </main>
     </div>
@@ -24,4 +22,3 @@ function Resource() {
 }
 
 export default Resource;
-
