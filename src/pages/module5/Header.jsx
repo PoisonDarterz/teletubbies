bb20d92dc899ae83bd552f1a7e4957654891b3f6
poisondarterz <jc.chuan.0303@gmail.com>
2024-04-02 00:01:00 +0800

    settle header1

--- src/pages/module5/Header.jsx
+++ src/pages/module5/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import './Header.css'; // Assuming you have a CSS file for styles
 
 function Header() {
     return (
-        <div className="header">
-            <img src="/Rectangle 38.png" alt="Your Description" style={{ width: '100%', height: 'auto' }} />
-            <div className="welcome-section">
-                <h1 className="welcome-text">Welcome back!</h1>
-                <h2 className="department-text">Software Engineering Department</h2>
+        <div className="relative">
+            <img src="/Rectangle 38.png" alt="Your Description" className="w-full h-auto" />
+            <div className="absolute top-8 left-2 flex flex-col items-start">
+                <h1 className="text-white text-8xl mt-2 ml-2">Welcome back!</h1>
+                <br></br>
+                <h2 className="text-white text-4xl ml-2">Software Engineering Department</h2>
             </div>
-            <div className="profile-info">
-                <img src="/Ellipse 1.png" alt="Profile" className="profile-pic" />
-                <p className="profile-name">Michael Lim</p>
+            <div className="absolute top-[80px] right-8 text-center transform -translate-y-1/2">
+                <img src="/Ellipse 1.png" alt="Profile" className="w-12 h-12 rounded-full mx-auto" />
+                <p className="mt-1 text-white">Michael Lim</p>
             </div>
         </div>
     );
