f32ef203976574975edfe2c51c79f71fb53691b4
Jingwennnn <wongjingwen1234@gmail.com>
2024-04-01 22:34:51 +0800

    scheduling

---
+++ src/pages/module5/engagement.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopNav from './TopNav'; 
+
+
+function Engagement() {
+  return (
+    <div>
+      <TopNav />
+      {/* rest of your code */}
+    </div>
+  );
+}
+
+export default Engagement;
--- src/pages/module5/pychart.jsx
+++ src/pages/module5/pychart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
+import { Link } from 'react-router-dom';
 
 function PyChart() {
   return (
@@ -14,18 +15,18 @@ function PyChart() {
           className="h-72"
           lineWidth={50} // Set the line width to control the thickness of the ring
         />
-          <img src="/image 6.png" alt="emoji" style={{ position: 'absolute', top: '22%', left: '-4%', height: '10%' }} />
+        <img src="/image 6.png" alt="emoji" style={{ position: 'absolute', top: '22%', left: '-4%', height: '10%' }} />
         <div style={{ position: 'absolute', top: '20%', left: '8%', backgroundColor: '#F3F3F3', padding: '10px', color: '#1CD571', borderRadius: '10px', display: 'flex', alignItems: 'center' }}>
           65%
         </div>
-          <img src="/image 173.png" alt="emoji" style={{ position: 'absolute', top: '30%', right: '-10%', height: '10%' }} />
+        <img src="/image 173.png" alt="emoji" style={{ position: 'absolute', top: '30%', right: '-10%', height: '10%' }} />
         <div style={{ position: 'absolute', top: '28%', right: '2%', backgroundColor: '#F3F3F3', padding: '10px', color: '#DD0101', borderRadius: '10px', display: 'flex', alignItems: 'center' }}>
           15%
         </div>
       </div>
-      <button style={{ backgroundColor: '#C6E3FF', color: '#292F65', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '10px', marginTop: '10px' }}>
+      <Link to="/engagement" style={{ backgroundColor: '#C6E3FF', color: '#292F65', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '10px', marginTop: '10px', textDecoration: 'none' }}>
         Check it out <span style={{ marginLeft: '10px', color: '#146c94' }}>→</span>
-      </button>
+      </Link>
     </div>
   );
 }
