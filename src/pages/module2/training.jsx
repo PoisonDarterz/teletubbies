e4fb29b84b875e8488265ee5d071b6f5d47871ab
yihanwong <wongyihan2003@gmail.com>
2024-04-02 01:12:02 +0800

    training

--- src/pages/module2/training.jsx
+++ src/pages/module2/training.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import Header from './header';
-import './training.css';
 
 function Training() {
-    return (
-      <div className="App">
-        <header className="header">
-        </header>
-        <Header/>
-        <div className="buttons-container">
-          <button className="button">Featured</button>
-          <button className="button">SAMPLE PROJECT</button>
-        </div>
-        <div className="content">
-        <div className="title">Software Engineering Training Hub</div>
-<div className="description">Empowering New Hires for Success</div>
-        </div>
+  return (
+    <div className="Training relative">
+      <header className="header">
+      </header>
+      <Header/>
+      <div className="buttons-container mt-0 ml-4 flex">
+      <button className="button mr-2" style={{ backgroundColor: '#F6F1F1', width: '179px', height: '60px' }}>Featured</button>
+      <button className="button" style={{ backgroundColor: '#F6F1F1', width: '179px', height: '60px' }}>Sample Project</button>
       </div>
-    );
-  }
-  
-  export default Training;
+      <div className="content text-center mt-48">
+      <div className="title font-poppins text-[60px] text-black">Software Engineering Training Hub</div>
+      <div className="description font-potta one text-lg text-black">
+  <span className="text-[#146C94]">Empowering</span> New Hires for <span className="text-[#146C94]">Success</span>
+</div>
+<button className="bg-[#0076CE] w-32 h-12 mt-4 rounded-md">Begin</button>
+</div>
+</div>
+  );
+}
+
+export default Training;
