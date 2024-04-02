b7e25d5b1b98e32e76061516c22e09957e6755f5
Jingwennnn <wongjingwen1234@gmail.com>
2024-04-02 23:45:05 +0800

    feedback page

--- src/pages/module3/TopNav.jsx
+++ src/pages/module3/TopNav.jsx
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 function TopNav() {
   return (
     <nav className="flex items-center justify-between px-6 py-4 text-lg" style={{ height: '90px', backgroundColor: '#0076ce' }}>
-      <h1 className="text-white">DELL Technologies</h1>
+        <div className="flex flex-col">
+          <h1 className="text-white">DELL Technologies</h1>
+          <h2 className="text-white text-sm">Onboarding</h2> 
+        </div>
       <div className="space-x-6">
         <Link to="/forum" className="text-white">Forum</Link>
         <Link to="/faq" className="text-white">FAQ</Link>
