bea9d51224a3d2b23a6996749d9eb4f3fcde8084
Jingwennnn <wongjingwen1234@gmail.com>
2024-04-03 05:43:12 +0800

    account

--- src/pages/module1/headerM1.jsx
+++ src/pages/module1/headerM1.jsx
@@ -6,10 +6,10 @@ function Header() {
     <nav className="flex items-center justify-between px-6 py-4 text-lg w-full" style={{ height: '90px', backgroundColor: '#0076ce' }}>
       <h1 className="text-white">DELL Technologies</h1>
       <div className="space-x-6">
-        <Link to="/resource" className="text-white">Profile</Link>
-        <Link to="/training" className="text-white">Account Setup</Link>
+        <Link to="/profile" className="text-white">Profile</Link>
+        <Link to="/account" className="text-white">Account Setup</Link>
         <Link to="/GeneralInfo" className="text-white">Information</Link>
-        <Link to="/scheduling" className="text-white">Office Tour</Link>
+        <Link to="/VRTour" className="text-white">Office Tour</Link>
       </div>
       <div className="flex items-center space-x-4">
         <div className="w-8 h-8 bg-white rounded-full"></div> {/* This is the profile icon. Replace it with your own icon. */}
